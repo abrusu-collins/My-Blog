d02e4d3d62e6646a322b8a4a4e8a606c262e7648
abrusu-collins <snillochemingson@gmail.com>
2022-11-25 14:11:40 +0000

    cool

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,9 @@
+let ans1 = document.querySelector(".ans1");
+let ans2 = document.querySelector(".ans2");
+let ans3 = document.querySelector(".ans3");
+let ans4 = document.querySelector(".ans4");
+
+let show1 = document.querySelector(".show1");
+let show2 = document.querySelector(".show2");
+let show3 = document.querySelector(".show3");
+let show4 = document.querySelector(".show4");
